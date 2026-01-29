eb2c788d36bd1ffc78c1056782354158d66e2ad4
szapol <szapobaa@gmail.com>
2026-01-29 10:41:05 +0100

    add api endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('books', BookController::class)->only(['index', 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->only(['store', 'update', 'destroy']);
+});
+
+Route::apiResource('authors', AuthorController::class)->only('index', 'show');
